5c47b2244872cb58cfdb0cf729bd95dd88896d17
Melquizedeque Almeida Brandão <85648775+melkbrandaodev@users.noreply.github.com>
2022-02-27 19:53:34 -0300

    Add files via upload

---
+++ tipoNumber.js
@@ -0,0 +1,13 @@
+//Tipo Number
+const meuNumero = 3;
+const primeiroNumero = 1;
+const segundoNumero = 2;
+const operacaoMatematica = primeiroNumero * segundoNumero;
+console.log(operacaoMatematica)
+//Ponto Flutuante
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuante = .5;
+const NovaOperacao = primeiroNumero/numeroPontoFlutuante
+console.log(NovaOperacao)
+const alura = 'Alura';
+console.log(alura * primeiroNumero)
