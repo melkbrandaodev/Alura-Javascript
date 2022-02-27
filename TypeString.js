f90b13763f0106db7cec1fcc97e67ddf3b194058
Melquizedeque Almeida Brandão <85648775+melkbrandaodev@users.noreply.github.com>
2022-02-27 20:57:52 -0300

    Add files via upload
    exercício de JavaScript com Strings

---
+++ TypeString.js
@@ -0,0 +1,13 @@
+const texto = "olá Mundo!";
+const texto2 = 'olá Mundo!';
+console.log(texto);
+console.log(texto2)
+const senha = 'senhaSuperSegura46!';
+console.log(senha);
+const StringNumeros = "34567";
+const citacao  = "Meu nome é ";
+//console.log(citacao);
+const meuNome = "Melquizedeque";
+console.log(citacao + meuNome)
+// template string ou template Literal
+//Concatenação(+)
