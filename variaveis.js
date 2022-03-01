ddad44bba25c5e2cb75829e673d1b0232d8297a5
Melquizedeque Almeida Brandão <85648775+melkbrandaodev@users.noreply.github.com>
2022-02-28 22:43:05 -0300

    Arquivos Adicionados

---
+++ variaveis.js
@@ -0,0 +1,37 @@
+//var
+//Programa para calcular a área de um retângulo
+
+/*var altura = 5;
+var comprimento = 7;
+
+area =  altura * comprimento;
+console.log("A área correspondente é "+area);
+var area;//Pode-se declarar a variável antes de declarar pois o JS vai fazer uma varredura*/
+
+//LET
+/*let forma = "retângulo";
+let altura = 5;
+let comprimento = 7;
+let area;
+if(forma === "retângulo"){
+    area = altura * comprimento;
+}else{
+    area = altura*comprimento/2;
+}
+
+console.log(area);*/
+ 
+const forma = "Triângulo";
+const altura = 5;
+const comprimento = 7;
+let area ;
+if(forma === "Quadrado"){
+    area = altura*comprimento;
+}else{
+    area = (altura* comprimento)/2;
+}
+
+
+console.log(area);
+
+
