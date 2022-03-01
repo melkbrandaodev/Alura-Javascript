7a00723d15c672ead99759c72f7977c99eaf405e
Melquizedeque Almeida Brandão <85648775+melkbrandaodev@users.noreply.github.com>
2022-02-28 22:35:06 -0300

    Aula sobre Truthy e Falsy

---
+++ truthyFalse.js
@@ -0,0 +1,14 @@
+const usuarioLogado = true;
+const contaPaga = false;
+
+//truthy falsy
+
+console.log(0 ==false);
+console.log(" " == false);
+console.log(1 == true);
+//null => vazio ou nada
+ let minhaVar;
+ let varNull =null;
+
+ console.log(typeof minhaVar);
+ console.log(typeof varNull);
