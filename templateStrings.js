f198c5be50d83c98b55549c8494524c250320e3d
Melquizedeque Almeida Brandão <85648775+melkbrandaodev@users.noreply.github.com>
2022-03-03 22:53:26 -0300

    Add files via upload

---
+++ templateStrings.js
@@ -0,0 +1,8 @@
+const nome = "Melk";
+
+
+const idade = 2022-1978;
+const cidadeNascimento = "Santo André"
+const apresentacao = `Meu nome é ${nome},tenho ${idade} anos e nasci em ${cidadeNascimento}.`;
+console.log(apresentacao);
+
